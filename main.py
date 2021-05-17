fbb77ef2362b4937409b8f761ba9ea36d36723e9
AlexVBodrov <alexvbodrov@mail.ru>
2021-05-17 18:10:03 +0300

    1 step. Main Windows

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello git")
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+import sys
+
+def application():
+    app = QApplication(sys.argv)
+    window = QMainWindow()
+
+    window.setWindowTitle("Тренажер печати")
+    window.setGeometry(300, 250, 500, 500)
+
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+
+    application()
