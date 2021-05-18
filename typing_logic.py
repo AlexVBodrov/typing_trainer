86bce5bc79390d1be172a6c312b213d73f9d7ccd
AlexVBodrov <alexvbodrov@mail.ru>
2021-05-18 22:55:25 +0300

    rename module logic add PIL add image

--- typing_logic.py
+++ typing_logic.py
@@ -1,5 +1,6 @@
 import random
 
+
 s_midl = "asdfghjkl"
 s_low = "zxcvbnm"
 s_upp = "qwertyuiop"
@@ -8,20 +9,20 @@ all_sim = "asdfghjklzxcvbnmqwertyuiop"
 list_letter = [s_midl, s_low, s_upp, all_sim]
 level = 0
 count_attempt = 16
-count_len = 10
+count_len = 2
 
 # random.sample(population, k) - список длиной k из последовательности population. Значение не повторяються.
 # psw = ''.join([random.choice(ls) for x in range(12)]) значения могут повторяться.
-for i in range(0, count_attempt):
-    s_question = ''.join([random.choice(list_letter[level]) for x in range(count_len)])
 
-    print(s_question)
-    s_ask = input("input string : ")
+for i in range(0, count_attempt):
+    s_question = ''.join([random.choice(list_letter[level]) for _ in range(count_len)])
+
+    print("      ", s_question)
+    s_ask = input("введите даный текст : \n      ")
 
     if s_ask == s_question:
         print("ok")
         count_attempt -= 1
-        # count_len += 1
 
         if count_attempt < 13:
             level = 1
